535c6e7ee53a4ee0d4943e1d74647a24a922beac
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-10-30 16:14:48 -0400

    fixed_point_quantization

--- ex1.py
+++ ex1.py
@@ -5,16 +5,15 @@ Task 1.1: Implement Fixed-Point Quantization
 import numpy as np
 
 def fixed_point_quantization(tensor: np.ndarray, bits: int) -> np.ndarray:
-    min_val = min(tensor)
-    max_val = max(tensor)
-    out = np.ndarray()
-    for n in tensor:
-        
+    scale = 2**(bits-1)
+    out = []
+    for x in tensor:
+        quantx = int(x*scale) 
+        out.append(quantx)
 
-        
-
-    
-
+    return np.array(out)
+                      
+                      
 tensor = np.array([0.5, -0.8, 0.1, 0.7, -0.5], dtype=np.float32)
 quantized_tensor = fixed_point_quantization(tensor, 8)
 print(quantized_tensor)
